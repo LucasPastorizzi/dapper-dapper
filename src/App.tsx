802390be67a26e4cb099adacd5fd4dc392be4087
Lucas Pastorizzi <Lucasdasilvapastorizzi@gmail.com>
2026-02-03 13:22:14 -0300

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import { CartProvider } from "@/contexts/CartContext";
 import Index from "./pages/Index";
 import Catalog from "./pages/Catalog";
@@ -21,19 +22,19 @@ const App = () => (
       <CartProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/catalogo" element={<Catalog />} />
-            <Route path="/produto/:id" element={<ProductDetail />} />
-            <Route path="/sobre" element={<About />} />
-            <Route path="/contato" element={<Contact />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/checkout" element={<Checkout />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+       <HashRouter>
+  <Routes>
+    <Route path="/" element={<Index />} />
+    <Route path="/catalogo" element={<Catalog />} />
+    <Route path="/produto/:id" element={<ProductDetail />} />
+    <Route path="/sobre" element={<About />} />
+    <Route path="/contato" element={<Contact />} />
+    <Route path="/auth" element={<Auth />} />
+    <Route path="/checkout" element={<Checkout />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</HashRouter>
+
       </CartProvider>
     </TooltipProvider>
   </QueryClientProvider>
