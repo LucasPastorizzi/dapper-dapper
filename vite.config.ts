a1e46834590b587f7aecc4d41de633435fb9cd27
Lucas Pastorizzi <Lucasdasilvapastorizzi@gmail.com>
2026-02-23 23:00:28 -0300

    Fix Images

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/dapper-dapper/',
+  base: '/EcommerceVirtual/',
   server: {
     host: "::",
     port: 8080,
